cea2f87e00492fd6e235a88d01064ee16261464f
Adam Romano <manoadamro.imp@gmail.com>
2019-01-12 12:50:09 +0000

    added code from old repo

---
+++ tests/rules/test_has_scopes.py
@@ -0,0 +1,15 @@
+import unittest
+
+from flapi_jwt.rules import HasScopes
+
+
+class HasScopesTest(unittest.TestCase):
+    def test_has_scopes(self):
+        token = {"scp": ["read:thing", "write:thing"]}
+        rule = HasScopes("read:thing", "write:thing")
+        self.assertTrue(rule(token))
+
+    def test_has_scopes_fails(self):
+        token = {"scp": ["read:thing"]}
+        rule = HasScopes("read:thing", "write:thing")
+        self.assertFalse(rule(token))
